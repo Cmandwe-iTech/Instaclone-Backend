46e02ddc2b8e0fde07ae950f648b1f0fe653465c
Chhatrapati Mandwe <mandwec12@gmail.com>
2022-12-18 00:57:36 +0530

    message

---
+++ index.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose")
+const express = require("express")
+mongoose.set('strictQuery', true)
+const routes = require("./routes")
+var cors = require('cors') 
+
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(cors(corsOptions))
+var corsOptions = {
+    origin: 'https://instareact10x.onrender.com',
+    optionsSuccessStatus: 200 
+  }
+app.use("/",routes)
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+mongoose.connect('mongodb+srv://Chhatrapati:mandwe@cluster0.u3j3emm.mongodb.net/?retryWrites=true&w=majority',
+{ useNewUrlParser: true, useUnifiedTopology: true },()=>{
+    console.log("connected to db")
+})
+app.listen(8000, () => console.log("App listening on port 8000"))
---
+++ routes.js
@@ -0,0 +1,49 @@
+const express = require("express")
+const router = express.Router()
+const uploader = require("./multer")
+const cloudinary = require("./cloudinary")
+const InstaPost = require("./model")
+const bodyParser = require("body-parser");
+
+router.use(bodyParser.json());
+
+router.get("/data", async (req, res) => {
+    try {
+        const getPost = await InstaPost.find().sort({Date:-1});
+        res.json(
+            getPost
+        )
+    }
+    catch (e) {
+        res.send.json({
+            status: "failed",
+            message: e.message
+        })
+
+    }
+
+})
+router.post("/form", uploader.single('file'), async (req, res) => {
+    console.log(req.body, req.file)
+    try {
+        const upload = await cloudinary.v2.uploader.upload(req.file.path);
+        const data = await InstaPost.insertMany(
+           { file: upload.secure_url,
+            Author: req.body.Author,
+            Location: req.body.Location,
+            Description: req.body.Description,
+            Likes: Math.ceil(Math.random()*1000),
+            Date: Date.now()}
+        );
+        res.json({
+            success: true,
+            result: data,
+        });
+    }
+    catch (e) {
+        return res.json({
+            err: e.message
+        });
+    }
+});
+module.exports = router;
