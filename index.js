59fcf3f0a7d7900da6921b584fb62b2fcb308250
Chhatrapati Mandwe <mandwec12@gmail.com>
2023-07-03 20:40:52 +0530

    changes

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
-const mongoose = require("mongoose")
-const express = require("express")
-mongoose.set('strictQuery', true)
-const routes = require("./routes")
-var cors = require('cors') 
+const mongoose = require("mongoose");
+const express = require("express");
+mongoose.set("strictQuery", true);
+var cors = require("cors");
 
 const bodyParser = require("body-parser");
+const router = require("./routes");
 
 const app = express();
 
-app.use(cors(corsOptions))
-var corsOptions = {
-    origin: 'https://instareact10x.onrender.com',
-    optionsSuccessStatus: 200 
-  }
-app.use("/",routes)  
+// var corsOptions = {
+//   origin: "https://instareact10x.onrender.com",
+//   optionsSuccessStatus: 200,
+// };
+app.use(cors());
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-mongoose.connect(process.env.Database_URL,{ useNewUrlParser: true, useUnifiedTopology: true },()=>{
-    console.log("connected to db")
-})
-app.listen(8000, () => console.log("App listening on port 8000"))
+mongoose.connect(
+  process.env.Database_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("connected to db");
+  }
+);
+app.use("/", router);
+app.listen(8000, () => console.log("App listening on port 8000"));
